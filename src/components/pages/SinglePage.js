75b78eb6745bdfe48e40263513689b23551d3c17
saifullahsiddique <info@saifullah.co>
2022-03-19 18:04:38 +0600

    single post page initialize

--- src/components/pages/SinglePage.js
+++ src/components/pages/SinglePage.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import {useParams} from "react-router-dom";
 
-function SinglePage() {
+function SinglePage(props) {
+    console.log(props)
+
+    const post_id = useParams()
+
     return (
         <div className="container">
             <h2>
-                Single Post Page
+                Single Post Page of the {post_id.postId}
             </h2>
         </div>
     )

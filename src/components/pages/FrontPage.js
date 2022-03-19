37ee028f0e8c28eb82edfef1ce1495083fa21ea9
saifullahsiddique <info@saifullah.co>
2022-03-19 12:15:53 +0600

    front-page add page header

--- src/components/pages/FrontPage.js
+++ src/components/pages/FrontPage.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../partials/Header'
 import Footer from '../partials/Footer'
+import PageHeader from '../partials/PageHeader'
 import FeaturedSection from '../sections/FeaturedSection'
 
 function FrontPage() {
     return (
         <div>
             <Header />
+            <PageHeader />
             <FeaturedSection />
             <Footer />
         </div>
